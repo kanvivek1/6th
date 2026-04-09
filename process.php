58c42d256b16023223e608019341ce4700ccd19e
Vivek Kanoujiya <kanvivek1@gmail.com>
2026-04-09 10:36:00 +0530

    Add process.php for user registration handling

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $_POST['name'];
+
+$email = $_POST['email'];
+
+$password = $_POST['password'];
+
+$gender = $_POST['gender'];
+
+echo "<h2>Registration Successful</h2>";
+
+echo "Name: " . $name . "<br>";
+
+echo "Email: " . $email . "<br>";
+
+echo "Password: " . $password . "<br>";
+
+echo "Gender: " . $gender . "<br>";
